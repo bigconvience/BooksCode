8c70af4c63e98afa094dba8c355aeda408cdeb43
james <jbp.130@163.com>
2022-02-23 22:22:55 +0800

    add count

---
+++ the_c_programming_language/01_word_count.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+int main() {
+	int c, nl, nw, nc, state = OUT;
+	nl = nw  = nc = 0;
+	while((c = getchar()) != EOF) {
+		++nc;
+		if (c == '\n') {
+			++nl;
+		}
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+		} else if (state == OUT) {
+			state = IN;
+			++nw;
+		}
+	}
+	printf("%d %d %d\n", nl, nw, nc);
+	return 0;
+}
