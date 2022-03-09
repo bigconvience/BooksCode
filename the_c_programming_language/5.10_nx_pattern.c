7998dcabe60330c9ab4c7e9b165f1f788cacb783
james <jbp.130@163.com>
2022-03-09 08:38:39 +0800

    -nx

---
+++ the_c_programming_language/5.10_nx_pattern.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int get_line(char *line, int max);
+
+int main(int argc, char *argv[]) {
+	char line[MAXLINE];
+	long lineno = 0;
+	int c, except = 0, number = 0, found = 0;
+
+	while(--argc > 0 && (*++argv)[0] == '-')
+		while(c = *++argv[0])
+			switch(c) {
+			case 'x':
+				except = 1;
+				break;
+			case 'n':
+				number = 1;
+				break;
+			default:
+				printf("find: illegal option %c\n", c);
+				argc = 0;
+				found = -1;
+				break;
+			}
+
+	if (argc != 1) 
+		printf("Usage: find -x -n pattern\n");
+	else 
+		while(get_line(line, MAXLINE) > 0) {
+			lineno++;
+			if ((strstr(line, *argv) != NULL) != except) {
+				if (number)
+					printf("%ld:", lineno);
+				printf("%s", line);
+				found++;
+			}
+		}
+	return found;
+}
