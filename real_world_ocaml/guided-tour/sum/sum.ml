37ed8040235d05b1376179cacc001beee971e865
bigconvience <jbp.130@163.com>
2022-10-05 14:20:15 +0800

    guided tour sum

---
+++ real_world_ocaml/guided-tour/sum/sum.ml
@@ -0,0 +1,11 @@
+open Base
+open Stdio
+
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
+let () =
+  printf "Total: %F\n" (read_and_accumulate 0.)
